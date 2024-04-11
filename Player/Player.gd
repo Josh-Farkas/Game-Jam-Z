db6df7784d79a0e71ddbaa98742a20f0c8437453
Arnimox <justreqofficial@gmail.com>
2024-04-11 17:28:54 -0400

    Git push test

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,17 +3,11 @@ extends CharacterBody2D
 const INV_SIZE = 10
 
 @export var speed = 100
-var speed_modifier = 1
-var inventory = [].resize(INV_SIZE)
+var speed_modifier: int = 1
+var inventory: Array = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	inventory.resize(INV_SIZE)
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down").normalized()
