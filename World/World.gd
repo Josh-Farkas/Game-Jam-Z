6d7c518b4441aa88c87908edf3c648a842e49206
Josh-Farkas <120142037+Josh-Farkas@users.noreply.github.com>
2024-04-12 18:57:43 -0400

    Add player group

--- World/World.gd
+++ World/World.gd
@@ -3,11 +3,11 @@ extends Node2D
 const MAP_SIZE = Vector2(10000, 10000)
 
 @onready
-var trees: Node2D = get_node("Trees")
+var objects: Node2D = get_node("Objects")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	trees.generate_trees(MAP_SIZE)
+	objects.generate_trees(MAP_SIZE)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
