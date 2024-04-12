1dcee5a9cf6ca0dca0a6e204b9db376256b7d2eb
Josh-Farkas <120142037+Josh-Farkas@users.noreply.github.com>
2024-04-12 17:52:01 -0400

    Merge from main test

--- Items/Item.gd
+++ Items/Item.gd
@@ -8,6 +8,13 @@ var item_types: Dictionary = {
 	
 }
 
+@onready
+var player = get_tree().get_first_node_in_group("Player")
+
+var player_in_range: bool = false
+var speed = 15
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -19,4 +26,14 @@ func set_type(name: String) -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if player_in_range:
-		position += position.direction_to(player)
+		position += position.direction_to(player) * speed * delta
+
+
+func _on_pickup_area_body_entered(body):
+	if body == player:
+		player_in_range = true
+
+
+func _on_pickup_area_body_exited(body):
+	if body == player:
+		player_in_range = false
