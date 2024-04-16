b5c57e1b05649e60288709f5269753a392101958
Josh-Farkas <120142037+Josh-Farkas@users.noreply.github.com>
2024-04-16 00:56:21 -0400

    Add ore veins

--- Player/Player.gd
+++ Player/Player.gd
@@ -50,6 +50,9 @@ func _physics_process(delta):
 	
 
 func _input(event):
+	if Input.is_action_just_pressed("dev_mode"):
+		$CollisionShape2D.disabled = true
+		speed = 500
 	if Input.is_action_just_pressed("left_click"):
 		use_item(inventory[current_slot])
 		
--- World/World.gd
+++ World/World.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const CHUNK_SIZE = Vector2(32*16, 32*16)
+const CHUNK_SIZE = Vector2(32*16, 20*16)
 
 const TILEMAP_COORDS := {
 	"Snow": Vector2i.ZERO,
@@ -13,6 +13,12 @@ const TILEMAP_COORDS := {
 	"Grass": Vector2i(2, 2),
 	"Coal": Vector2i(1,3),
 	"Water": Vector2i(3,0),
+	"Iron": Vector2i(2,3),
+	"Uranium": Vector2i(3,3),
+}
+
+const DENSITIES: Dictionary = {
+	"Tree": 0.2, "Rock": .075, "Coal":.015, "Grass": 0.3, "Iron":0.02, "Uranium":0.01,
 }
 
 var item_scn = preload("res://Items/Item.tscn")
@@ -25,28 +31,31 @@ var selection_coords = Vector2i.ZERO
 
 var generated_chunks: Dictionary = {}
 
-var noise := FastNoiseLite.new()
+var seed: int = 1
+var forest_noise := FastNoiseLite.new()
 var river_noise := FastNoiseLite.new()
 var ore_noise := FastNoiseLite.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	noise.seed = randi_range(0, 1000)
-	noise.noise_type = FastNoiseLite.TYPE_PERLIN
-	noise.fractal_gain = 0
+	if seed == 0: seed = randi_range(1,1000)
+	forest_noise.seed = seed
+	forest_noise.noise_type = FastNoiseLite.TYPE_PERLIN
+	forest_noise.fractal_gain = 0
 	
-	river_noise.seed = noise.seed + 1
+	river_noise.seed = forest_noise.seed + 1
 	river_noise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
 	river_noise.fractal_gain = 0
 	river_noise.fractal_octaves = 2
 	
-	ore_noise.seed = noise.seed + 1
-	ore_noise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+	ore_noise.seed = forest_noise.seed + 2
+	ore_noise.noise_type = FastNoiseLite.TYPE_CELLULAR
 	ore_noise.fractal_gain = 0
 	ore_noise.fractal_octaves = 2
 	
-	noise.get_seamless_image(1000, 1000).save_jpg("res://World/Noise.jpg")
-	river_noise.get_seamless_image(1000, 1000).save_jpg("res://World/RiverNoise.jpg")
+	forest_noise.get_seamless_image(1000, 1000).save_jpg("res://World/NoiseImages/Noise.jpg")
+	river_noise.get_seamless_image(1000, 1000).save_jpg("res://World/NoiseImages/RiverNoise.jpg")
+	ore_noise.get_seamless_image(1000, 1000).save_jpg("res://World/NoiseImages/OreNoise.jpg")
 	
 	
 	generate_chunks(Vector2i.ZERO)
@@ -67,7 +76,6 @@ func _process(delta):
 		selection_coords = coords
 	
 	var new_chunk: Vector2i = floor(player.global_position / CHUNK_SIZE)
-	print(new_chunk)
 	if new_chunk != player_chunk:
 		generate_chunks(new_chunk)
 		player_chunk = new_chunk
@@ -96,34 +104,43 @@ func generate_chunks(center_pos: Vector2i):
 	for x in range(-1, 2):
 		for y in range(-1, 2):
 			var chunk_pos := center_pos + Vector2i(x, y)
-			print(chunk_pos)
 			if not generated_chunks.get(chunk_pos, false):
-				print("Generating: ", chunk_pos)
-				generate_objects(CHUNK_SIZE, chunk_pos, {"Tree": 0.2, "Rock": .075, "Coal":.015, "Grass": 0.2}, 16, 5)
+				generate_objects(CHUNK_SIZE, chunk_pos, DENSITIES, 16, 5, 20, 20)
 				generated_chunks[chunk_pos] = true
 				
 
-func spawn_obj(tile_coords: Vector2i, pos: Vector2, layer:int =1):
-	tilemap.set_cell(layer, tilemap.local_to_map(pos), 0, tile_coords)
+func spawn_obj(tile_coords: Vector2i, pos: Vector2, layer:int = 1, alt: int = 0):
+	tilemap.set_cell(layer, tilemap.local_to_map(pos), 0, tile_coords, alt)
 
 
-func generate_objects(size: Vector2, offset: Vector2i, densities: Dictionary, tile_size: int = 16, forest_scale: float = 3, river_scale = 20):
+func generate_objects(size: Vector2, offset: Vector2i, densities: Dictionary, tile_size: int = 16, forest_scale: float = 3, river_scale: float = 20, ore_scale: float = 5):
 	for x in range(offset.x * size.x, size.x + offset.x * size.x, tile_size):
 		for y in range(offset.y * size.y, size.y + offset.y * size.y, tile_size):
 			var pos = Vector2i(x, y)
 			# value of the perlin noise scaled by forest_scale
-			var odds := clampf(noise.get_noise_2d(x / forest_scale, y / forest_scale) + 0.5, 0, 1)
-			var river_val: float = river_noise.get_noise_2d(x / river_scale, y / river_scale)
-			if abs(river_val) < .1: # River thickness
+			var forest_odds := clampf(forest_noise.get_noise_2d(x / forest_scale, y / forest_scale) + 0.5, 0, 1)
+			var river_odds: float = river_noise.get_noise_2d((x + 20*16) / river_scale, (y+20*16) / river_scale)
+			var ore_odds: float = -ore_noise.get_noise_2d(x / ore_scale, y / ore_scale)
+			
+			if abs(river_odds) < .1: # River thickness
 				spawn_obj(TILEMAP_COORDS.Water, pos, 0)
-			else:
+				
+			else: # not a water tile
 				spawn_obj(TILEMAP_COORDS.Snow, pos, 0)
-				if odds * densities.Tree >= randf():
+
+				if forest_odds * densities.Tree >= randf():
 					if randf() < .95: spawn_obj(TILEMAP_COORDS.Tree, pos)
 					else: spawn_obj(TILEMAP_COORDS.DeadTree, pos)
-				elif odds * densities.Rock >= randf():
+				elif forest_odds * densities.Rock >= randf():
 					spawn_obj(TILEMAP_COORDS.Stone, pos)
-				elif odds * densities.Coal >= randf():
+				elif forest_odds * densities.Coal >= randf():
 					spawn_obj(TILEMAP_COORDS.Coal, pos)
-				elif (1-odds) * densities.Grass >= randf():
-					spawn_obj(TILEMAP_COORDS.Grass, pos)
+				elif (1 - forest_odds) * densities.Grass >= randf():
+					spawn_obj(TILEMAP_COORDS.Grass, pos, 1, randi_range(0, 1))
+					# generate ore, go rarest -> least rare
+				elif ore_odds >= 1 - densities.Uranium and randf() > .5:
+					spawn_obj(TILEMAP_COORDS.Uranium, pos)
+				elif ore_odds >= 1 - densities.Iron and randf() > .5:
+					spawn_obj(TILEMAP_COORDS.Iron, pos)
+					print("Iron Spawned At ", pos)
+					print("Player Pos, ", player.position)
